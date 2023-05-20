4fc77b38851c61f2b862e585ad35c2b8762e4685
Alireza-WSLLinux <najafi73alireza@gmail.com>
2023-05-20 18:19:56 +0330

    wip table defintitions

--- STMud/packages/contracts/mud.config.ts
+++ STMud/packages/contracts/mud.config.ts
@@ -6,5 +6,99 @@ export default mudConfig({
       keySchema: {},
       schema: "uint32",
     },
+    //Agent
+	  IsAgent: {
+      keySchema: {
+        entity: "bytes32",
+      },
+      schema: "bool",
+    },
+    
+    //Movement
+    MovementCost: {
+      keySchema: {
+        entity: "bytes32",
+      },
+      schema: "uint32",
+    },
+    Altitude: {
+      keySchema: {
+        entity: "bytes32",
+      },
+      schema: "uint32",
+    },
+    //Stats
+    MaxHP: {
+      keySchema: {
+        entity: "bytes32",
+      },
+      schema: "uint32",
+    },
+    MoveSpeed: {
+      keySchema: {
+        entity: "bytes32",
+      },
+      schema: "uint32",
+    },
+    Strength: {
+      keySchema: {
+        entity: "bytes32",
+      },
+      schema: "uint32",
+    },
+    Recovery: {
+      keySchema: {
+        entity: "bytes32",
+      },
+      schema: "uint32",
+    },
+    Weight: {
+      keySchema: {
+        entity: "bytes32",
+      },
+      schema: "uint32",
+    },
+    //State
+	  ActionRecovery: {
+      keySchema: {
+        entity: "bytes32",
+      },
+      schema: "uint32",
+    },
+    CurrentHP: {
+      keySchema: {
+        entity: "bytes32",
+      },
+      schema: "uint32",
+    },
+    //Item
+    IsItem: {
+      keySchema: {
+        entity: "bytes32",
+      },
+      schema: "bool",
+    },
+    IsEquipment: {
+      keySchema: {
+        entity: "bytes32",
+      },
+      schema: "bool",
+    },
+    OwnedBy: {
+      keySchema: {
+        entity: "bytes32",
+      },
+      schema: "bytes32",
+    },
+    Position: {
+      keySchema: {
+        entity: "bytes32",
+      },
+      schema: {
+        x: "uint32",
+        y: "uint32",
+      },
+    }
+    
   },
 });
