e90449c6d1c83c437359bb51c54093f5459f0c7d
a-najafi <najafi73alireza@gmail.com>
2023-05-21 18:27:10 +0330

    fix: missing IWorld include

--- STMud/packages/contracts/src/libraries/LibMap.sol
+++ STMud/packages/contracts/src/libraries/LibMap.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.8.0;
 
 import { IsObstruction, MovementCost } from "../codegen/Tables.sol";
 import { Position, PositionTableId } from "../codegen/tables/Position.sol";
+import { IWorld } from "../codegen/world/IWorld.sol";
 import { getKeysWithValue } from "@latticexyz/world/src/modules/keyswithvalue/getKeysWithValue.sol";
 
 library LibMap {
